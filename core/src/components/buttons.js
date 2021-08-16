ae96fe94886047b098db03c34e0daec26af3a15f
shivanya-chandra <shivanyachandra2469@gmail.com>
2021-08-16 23:14:14 +0530

    final touches

---
+++ core/src/components/buttons.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Footer } from "./footer";
+
+export const Buttons = () => {
+	return (
+		<div className="buttons-flex">
+			<button className="btn-1">Find Nearest Store</button>
+			<button className="btn-2">Order Online</button>
+		</div>
+	);
+};
--- core/src/index.js
+++ core/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import { Power } from './components/power';
-import { Aerodynamics } from './components/aerodynamics';
-import { Dash } from './components/dash';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { Power } from "./components/power";
+import { Aerodynamics } from "./components/aerodynamics";
+import { Dash } from "./components/dash";
+import { Buttons } from "./components/buttons";
+import { Footer } from "./components/footer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Power />
-    <Aerodynamics />
-    <Dash />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+		<Power />
+		<Aerodynamics />
+		<Dash />
+		<Buttons />
+		<Footer />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
